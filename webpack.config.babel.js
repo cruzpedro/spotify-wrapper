34bc05674841a470864684f014139b5c3a47cae3
Pedro Cruz <cruz.pedro22.11@gmail.com>
2018-06-15 11:10:41 -0300

    Webpack integration

---
+++ webpack.config.babel.js
@@ -0,0 +1,21 @@
+import { join } from 'path';
+
+const nodeENV = process.env.NODE_ENV || 'production';
+
+const include = join(__dirname, 'src');
+
+export default {
+  mode: nodeENV,
+  devtool: 'source-map',
+  entry: './src/index',
+  output: {
+    path: join(__dirname, 'dist'),
+    libraryTarget: 'umd',
+    library: 'spotifyWrapper',
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, use: 'babel-loader', include },
+    ],
+  },
+};
