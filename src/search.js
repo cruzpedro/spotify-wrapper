2f049d623a6bd37dcf02fa0e634a9a67dabd0144
Pedro Cruz <cruz.pedro22.11@gmail.com>
2018-06-15 15:49:24 -0300

    Add token api

--- src/search.js
+++ src/search.js
@@ -1,12 +1,12 @@
-import API_URL from './config';
+import { API_URL, HEADERS } from './config';
 import toJSON from './utils';
 
-export const search = (query, type) => fetch(`${API_URL}/search?q=${query}&type=${type}`).then(toJSON);
+export const search = (query, type) => fetch(`${API_URL}/search?q=${query}&type=${type}`, HEADERS).then(toJSON);
 
-export const searchAlbums = query => search(query, 'album');
+export const searchAlbums = query => search(query, 'album', HEADERS);
 
-export const searchArtists = query => search(query, 'artist');
+export const searchArtists = query => search(query, 'artist', HEADERS);
 
-export const searchTracks = query => search(query, 'track');
+export const searchTracks = query => search(query, 'track', HEADERS);
 
-export const searchPlaylists = query => search(query, 'playlist');
+export const searchPlaylists = query => search(query, 'playlist', HEADERS);
